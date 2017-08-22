d79bafe1f257a540b973fabc5bc6199295b3496f
Nardberjean <Nardberjean@users.noreply.github.com>
2017-08-22 06:50:57 -0400

    Create EngineAsArgumentControllerTest.php

---
+++ tests/EngineAsArgumentControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the Symfony Util package.
+ *
+ * (c) Jean-Bernard Addor
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use SymfonyUtil\Controller\EngineAsArgumentController;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \SymfonyUtil\Controller\EngineAsArgumentController
+ */
+final class EngineAsArgumentControllerTest extends TestCase
+{
+    public function testCanBeCreated()
+    {
+        $this->assertInstanceOf(
+            EngineAsArgumentController::class,
+            new EngineAsArgumentController()
+        );
+    }
+
+    /*
+    public function testReturnsResponse()
+    {
+        $this->assertInstanceOf(
+            Table::class,
+            (new SchemaBuilder(new Schema()))->userTable()
+        );
+    }
+    */
+}
