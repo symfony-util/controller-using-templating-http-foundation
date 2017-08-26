719bdff68fe868a53a5bce7d5fbd60db7b202e7e
Nardberjean <Nardberjean@users.noreply.github.com>
2017-08-26 13:59:36 -0400

    Create VariadicControllerTest.php

---
+++ tests/VariadicControllerTest.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * This file is part of the Symfony-Util package.
+ *
+ * (c) Jean-Bernard Addor
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Bridge\Twig\TwigEngine;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Templating\TemplateNameParser;
+use SymfonyUtil\Controller\TemplatingController;
+
+/**
+ * @covers \SymfonyUtil\Controller\TemplatingController
+ */
+final class TemplatingControllerTest extends TestCase
+{
+    public function testCanBeCreated()
+    {
+        $this->assertInstanceOf(
+            // ::class, // 5.4 < php
+            'SymfonyUtil\Controller\TemplatingController',
+            new TemplatingController(new TwigEngine(
+                new Twig_Environment(new Twig_Loader_Array(['index.html.twig' => 'Hello World!'])),
+                new TemplateNameParser()
+            ))
+        );
+    }
+
+    public function testEmptyReturnsResponse()
+    {
+        $this->assertInstanceOf(
+            // Response::class, // 5.4 < php
+            'Symfony\Component\HttpFoundation\Response',
+            (new TemplatingController(new TwigEngine(
+                new Twig_Environment(new Twig_Loader_Array(['index.html.twig' => 'Hello World!'])),
+                new TemplateNameParser()
+            )))->__invoke()
+        );
+    }
+
+    public function testArrayReturnsResponse()
+    {
+        $this->assertInstanceOf(
+            // Response::class, // 5.4 < php
+            'Symfony\Component\HttpFoundation\Response',
+            (new TemplatingController(new TwigEngine(
+                new Twig_Environment(new Twig_Loader_Array([
+                    'index.html.twig' => '<ul>{% for item in 0 %}<li>{{ item }}</li>{% endfor %}</ul>',
+                ])),
+                new TemplateNameParser()
+            )))->__invoke([
+                'One',
+                'Two',
+                'Three',
+            ])
+        );
+    }
+}
+
+// http://api.symfony.com/3.3/Symfony/Bridge/Twig/TwigEngine.html
+// http://api.symfony.com/3.3/Symfony/Bundle/TwigBundle/TwigEngine.html
