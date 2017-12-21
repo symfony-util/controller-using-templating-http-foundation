46eb2faebf24315deaca228f0abe19d2e7725883
Nardberjean <Nardberjean@users.noreply.github.com>
2017-12-20 19:29:04 -0500

    Create testing_dependencies_install.php

---
+++ scripts/testing_dependencies_install.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Symfony-Util package.
+ *
+ * (c) Jean-Bernard Addor
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Symfony\Component\Process\ProcessBuilder;
+
+if (7 <= PHP_MAJOR_VERSION)
+{
+(new ProcessBuilder(['composer', 'global require --dev phpstan/phpstan-phpunit']))->getProcess()->run();
+}
+else
+{
+(new ProcessBuilder(['composer', 'global require --dev phpunit/phpunit']))->getProcess()->run();
+}
