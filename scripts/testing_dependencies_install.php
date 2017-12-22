08de1463447f4f01294a98d399b02bf47c5df2f2
Nardberjean <Nardberjean@users.noreply.github.com>
2017-12-22 11:09:25 -0500

    Update testing_dependencies_install.php

--- scripts/testing_dependencies_install.php
+++ scripts/testing_dependencies_install.php
@@ -15,7 +15,7 @@ require __DIR__.'/vendor/autoload.php';
 
 function shellLikeExec($s)
 {
-    // (new Process(explode(' ', $s)))->mustRun(function ($type, $buffer) {
+    // (new Process(explode(' ', $s)))->mustRun(function ($type, $buffer) { // This may be required for Symfony 4!
     (new Process($s))->mustRun(function ($type, $buffer) {
         if (Process::ERR === $type) {
             fwrite(STDERR, $buffer);
