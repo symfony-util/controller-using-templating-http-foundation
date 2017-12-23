dfe6eaacbd6b5ad5fdd16ffbee76891238a41970
Nardberjean <Nardberjean@users.noreply.github.com>
2017-12-23 11:19:17 -0500

    Update install-manager.php

--- scripts/install-manager.php
+++ scripts/install-manager.php
@@ -2,6 +2,8 @@
 // use Acme\Console\Command\HelloWorldCommand;
 use Symfony\Component\Console\Application;
 
+require __DIR__.'/vendor/autoload.php';
+
 $command = new InstallPhpstanPhpUnitCommand();
 $application = new Application();
 $application->add($command);
