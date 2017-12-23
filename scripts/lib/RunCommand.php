a74b9e04301e5dc8601e45f8d908e9f1526810db
Nardberjean <Nardberjean@users.noreply.github.com>
2017-12-23 16:34:34 -0500

    Create RunCommand.php

---
+++ scripts/lib/RunCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the Symfony-Util package.
+ *
+ * (c) Jean-Bernard Addor
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class RunCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('run')
+            ->setDescription('Command line run from Process')
+            ->addArgument(
+                'commandline',
+                InputArgument::IS_ARRAY | InputArgument::REQUIRED,
+                'The command line'
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $helper = $this->getHelper('process'); //! https://symfony.com/doc/4.0/components/console/helpers/processhelper.html
+        $helper->run($output, $input->getArgument('names'));
+    }
+}
