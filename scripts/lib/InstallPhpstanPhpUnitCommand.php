b6e0c6ef5e25b236e63c15143e964a3a51265870
Nardberjean <Nardberjean@users.noreply.github.com>
2017-12-25 09:17:21 -0500

    Update InstallPhpstanPhpUnitCommand.php

--- scripts/lib/InstallPhpstanPhpUnitCommand.php
+++ scripts/lib/InstallPhpstanPhpUnitCommand.php
@@ -12,6 +12,7 @@
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+
 // Symfony\Component\Process hidden dependecy
 
 class InstallPhpstanPhpUnitCommand extends Command
