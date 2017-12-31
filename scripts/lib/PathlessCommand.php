e2bd0a147e755e3bf8b9f6e8c762413fdc0e6b8d
Nardberjean <Nardberjean@users.noreply.github.com>
2017-12-31 08:24:13 -0500

    Create PathlessCommand.php

---
+++ scripts/lib/PathlessCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the Symfony-Util package.
+ *
+ * (c) Jean-Bernard Addor
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+// Symfony\Component\Process hidden dependecy
+
+class PathlessCommand extends Command
+{
+    const NAME = 'run';
+    const DESCRIPTION = 'Command line run from Process';
+
+    protected function configure()
+    {
+        $this->setName(self::NAME)
+            ->setDescription(self::DESCRIPTION)
+            ->addArgument(
+                'commandline',
+                InputArgument::IS_ARRAY | InputArgument::REQUIRED,
+                'The command line'
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $helper = $this->getHelper('process'); //! https://symfony.com/doc/4.0/components/console/helpers/processhelper.html
+        // This introduces a hidden dependecy on symfony/process!
+
+        return $helper->run($output, $input->getArgument('commandline'))->getExitCode();
+    }
+}
